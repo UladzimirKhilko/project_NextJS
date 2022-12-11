f0fcadb13f96e3e5430a53136c3c8798aa3c6150
Uladzimir Khilko <vladimirhilko@gmail.com>
2022-12-11 20:24:58 +0300

    husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,10 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import ym from 'react-yandex-metrika';
 import { YMInitializer } from 'react-yandex-metrika';
+import { Router } from 'next/router';
 
 export default function App({ Component, pageProps, router }: AppProps): JSX.Element  {
-  router.events.on('routeChangeComplete', (url: string) => {
+  Router.events.on('routeChangeComplete', (url: string) => {
     if (typeof window !== 'undefined') {
       ym('hit', url);
     }
@@ -21,8 +22,8 @@ export default function App({ Component, pageProps, router }: AppProps): JSX.Ele
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link rel="preconnect" href="https://mc.yandex.ru" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"/>
-        <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath } />
-        <meta property='og:locale' content='ru_RU' />
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+	      <meta property="og:locale" content="ru_RU" />
       </Head>
       < YMInitializer 
           accounts={[]}
